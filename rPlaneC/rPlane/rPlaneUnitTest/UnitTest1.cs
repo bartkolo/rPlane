4b33bf67483915a028d40f7b706604b9f7c2cf66
barney1526 <barney1526@gmail.com>
2016-12-12 16:33:29 +0100

    + Unitests + Plane library

---
+++ rPlaneC/rPlane/rPlaneUnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using rPlaneLibrary;
+
+namespace rPlaneUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AdsbMessage msg = new AdsbMessage("8D4840D6202CC371C32CE0576098");
+            Console.WriteLine(msg.TestMessage);
+
+
+
+            Assert.AreEqual(1,1);
+        }
+    }
+}
